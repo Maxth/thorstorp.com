a284f267c661be53e5127dd0b9776577279a2c99
Max <maxthorstorp@gmail.com>
2021-03-25 09:28:21 +0100

    lagt till spin och färg på 'code'

--- JS/index.js
+++ JS/index.js
@@ -8,16 +8,16 @@ console.log(ul);
 let showInOrder = () => {
     setTimeout(()=> {
         comma.classList.remove('hidden')
-    }, 1600)
+    }, 1650)
     setTimeout(()=> {
         namn.classList.remove('hidden')
-    }, 3400)
+    }, 3450)
     setTimeout(()=> {
         presentation.classList.remove('hidden')
-    }, 5500)
+    }, 6050)
     setTimeout(()=> {
         ul.classList.remove('hidden')
-    }, 7000)
+    }, 7550)
 
 }
 
